cdbe2235c09d7178b4bf502d9814b6b5bcb1cb9b
Alisson Felipe <alissonfelipenettt@gmail.com>
2023-01-17 20:35:15 -0300

    :lipstick: feat: [server] habit creation and details of the day (full habits/possible)

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,69 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { prisma } from "./lib/prisma";
+import dayjs from "dayjs";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get("/", async () => {
-    const habits = await prisma.habit.findMany();
+  app.post("/habits", async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().int().min(0).max(6)),
+    });
 
-    return habits;
+    const { title, weekDays } = createHabitBody.parse(request.body);
+
+    const today = dayjs().startOf("day").toDate();
+
+    await prisma.habit.create({
+      data: {
+        title,
+        createdAt: today,
+        weekDays: {
+          create: weekDays.map((weekDay) => ({ weekDay })),
+        },
+      },
+    });
+  });
+
+  app.get("/day", async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(),
+    });
+
+    const { date } = getDayParams.parse(request.query);
+
+    const parsedDate = dayjs(date).startOf("day");
+    const weekDay = parsedDate.get("day");
+
+    const possibleHabbits = await prisma.habit.findMany({
+      where: {
+        createdAt: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            weekDay,
+          },
+        },
+      },
+    });
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      },
+    });
+
+    const completedHabbits = day?.dayHabits.map(
+      (dayHabbit) => dayHabbit.habitId
+    );
+
+    return {
+      possibleHabbits,
+      completedHabbits,
+    };
   });
 }
